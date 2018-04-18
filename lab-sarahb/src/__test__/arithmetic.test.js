b00eb4fddd68de771b3ff38ee14007012c5dcf89
sarasaurus <bixler.s.e@gmail.com>
2018-04-17 17:07:57 -0700

    added readme and tests

--- lab-sarahb/src/__test__/arithmetic.test.js
+++ lab-sarahb/src/__test__/arithmetic.test.js
@@ -2,6 +2,20 @@
 
 const arithmetic = require('../lib/arithmetic.js');
 
-test('Should return hello name', () => {
-  expect(arithmetic.hi('poop')).toEqual('hello poop');
+describe('tests for the add method', () => {
+  test('both arguments should be numbers', () => {
+    expect(arithmetic.add('string', 2)).toEqual(null);
+  });
+  test('should return sum of the arguments', () => {
+    expect(arithmetic.add(1, 2)).toEqual(3);
+  });
+});
+
+describe('tests for the subtract method', () => {
+  test('both arguments should be numbers', () => {
+    expect(arithmetic.sub('string', 2)).toEqual(null);
+  });
+  test('should return second argument substract from first argument', () => {
+    expect(arithmetic.sub(5, 2)).toEqual(3);
+  });
 });
